8c0d2e3899dbdea9770dbc6c13171295a2d6d40f
LeraPopescu <valeriapopescu2000@yahoo.com>
2020-01-18 16:52:24 +0100

    map

--- mobile/src/app/home/home.page.ts
+++ mobile/src/app/home/home.page.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
 
+import { LoadingController, AlertController, Platform } from '@ionic/angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  MarkerCluster,
+  Marker,
+  MarkerOptions,
+  GoogleMapsAnimation,
+  MyLocation,
+  CameraPosition
+} from "@ionic-native/google-maps";
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +20,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  
+  map: GoogleMap;
+  constructor(
+    
+    private platform: Platform,
+  ) {}
+
+  async ngOnInit() {
+    await this.platform.ready();
+    await this.loadMap();
+  }
+  loadMap() {
+
+    /// Create map
+    this.map = GoogleMaps.create('map_canvas');
+
+    /// Go to my location
+  //  this.onButtonClick();
+  }
 
 }
